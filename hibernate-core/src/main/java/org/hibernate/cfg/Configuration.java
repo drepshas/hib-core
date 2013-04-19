0088d9693c0e699f3f1567548ccf9d4cff253913
Dave Repshas <David.Repshas@Teradata.com>
2013-04-18 17:32:09 -0700

    HHH-8191 Teradata

--- hibernate-core/src/main/java/org/hibernate/cfg/Configuration.java
+++ hibernate-core/src/main/java/org/hibernate/cfg/Configuration.java
@@ -1087,6 +1087,15 @@ public class Configuration implements Serializable {
 					while ( subIter.hasNext() ) {
 						ForeignKey fk = (ForeignKey) subIter.next();
 						if ( fk.isPhysicalConstraint() ) {
+							if (dialect.constraintRequiresFunction()) {
+								String alterCmd = fk.sqlCreateString(dialect, mapping, defaultCatalog, defaultSchema);
+								String[] cmds = dialect.generateConstraintFunctionSql(alterCmd, fk.getTable().getName(), fk.getName());
+								for (int i = 0; i < cmds.length; i++)
+									script.add(cmds[i]);
+							} else {
+
+
+
 							script.add(
 									fk.sqlCreateString(
 											dialect, mapping,
@@ -1097,7 +1106,7 @@ public class Configuration implements Serializable {
 						}
 					}
 				}
-
+				}
 			}
 		}
 
